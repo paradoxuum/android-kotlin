94c41cdcd61e9e8f67f3dfa5c2b6c2aa9c4dcf23
Eduardo <haevorak@gmail.com>
2018-09-01 11:39:03 -0300

    override onStart...OnDestroy

--- app/src/main/java/com/example/aluno/android_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/aluno/android_kotlin/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.aluno.android_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("onStart", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("onResumo", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("onPause", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("onStop", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("onDestroy", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("onRestart", "onRestart")
     }
 }
